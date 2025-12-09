b2ea813c11a470ce79e2bb0f1b883e5c8243669a
Santhosh <santhoshsangili.2802@gmail.com>
2025-12-09 17:06:37 +0530

    cross origin update

--- src/main/java/com/portfolioapi/rest/controller/ChatController.java
+++ src/main/java/com/portfolioapi/rest/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import dto.BaseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/chat")
 @Log4j2
